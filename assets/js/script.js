db89c2b925a58da557e3e73e1d2657ea6a703c8d
mcelhatton <colinm@mobiledealerdata.com>
2022-03-30 11:15:17 -0500

    saving working project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,30 @@
-var schedule = {
-  hour8 : '',
-  hour9: '',
-  hour10: '',
-  hour11: '',
-  hour12: '',
-  hour13: '',
-  hour14: '',
-  hour15: '',
-  hour16: '',
-};
-var array = [
-  {
-  hour8 : 'meeting'
-},
-];
-// save textarea function
+
+var timeNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+$('#currentDay').replaceWith(timeNow);
+
+console.log(timeNow);
+
+
+// save textarea to local storage function
 $('.saveBtn').on('click', function handleClick(event){
+
   var saveButton = $(this).siblings('.description').attr('id');
   var textInput = $(this).siblings('.description').val().trim();
 
   localStorage.setItem(saveButton, textInput);
-  console.log(saveButton);
-  console.log(textInput);
 
 });
 
-// load schedule function
+// load schedule from local storage loop 
 for (var i = 8; i < 18; i++) {
   $(`#hour${i}`).val(localStorage.getItem(`hour${i}`));
+  var currentHour = moment().hour();
+  if (i < currentHour) {
+    $(`#${i}hour`).addClass('past');
+  } else if (i === currentHour) {
+    $(`#${i}hour`).addClass('present');
+  } else if (i > currentHour) {
+    $(`#${i}hour`).addClass('future');
+  }
 }
 
